5e3576e9a29c4b078bd0d7fd8953951233277865
Esraa <jbesraa@gmail.com>
2016-11-10 15:16:24 +0000

    creat server

---
+++ src/handler.js
@@ -0,0 +1,10 @@
+var dictionary;
+
+var readDictionary = require('./readDictionary.js').readDictionary;
+dictionary = [];
+module.exports = dictionary;
+
+var handler = function(request,response){
+
+}
+module.export = handler;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
-var dictionary;
+var http = require('http');
+var handler = require('handler.js');
+var server = http.createServer(handler);
 
-var readDictionary = require('./readDictionary.js').readDictionary;
-dictionary = [];
-module.exports = dictionary;
+server.listen(3000, function(){
+  console.log("The server is listening to port 3000, ready to accept requests");
+});
