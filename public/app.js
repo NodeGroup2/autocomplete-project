a1f7e02426c66f352e198e49718b8fb5a6c57b50
msachi <marchee88@gmail.com>
2016-11-10 14:58:41 +0000

    Update li items when typing using mockMatch, add toggle effect

--- public/app.js
+++ public/app.js
@@ -1,28 +1,36 @@
 var inputField = document.querySelector(".input_field");
 
-// var suggestionElements = document.querySelectorAll(".suggestion_element");
+var suggestionElements = document.querySelectorAll(".suggestion_element");
 // for (var i = 0; i < suggestionElements.length; i++) {
 //   var suggestion = suggestion[i];
-//   button.addEventListener("click", function() {
+//   suggestion.addEventListener("click", function() {
 //     handleInput(this.id);
 //   });
 // };
 
-
 inputField.onkeyup = function() {
-  sendRequest();
-  showSuggestionList();
+  if (inputField.value === '') hideSuggestionList();
+  else {
+    sendRequest();
+    showSuggestionList();
+  }
 }
 
 function showSuggestionList() {
   document.querySelector(".suggestion_container").classList.remove("closed");
 }
 
+function hideSuggestionList() {
+  document.querySelector(".suggestion_container").classList.add("closed");
+}
+
 function sendRequest() {
   var input = inputField.value.split(' ');
-  var inputLast = input[input.length-1];
+  var inputLast = input.pop();
+  var inputSaved = input.join(' ');
   var url = '/' + inputLast;
   var xhr = new XMLHttpRequest();
+  updateDOM(inputSaved);
   xhr.addEventListener('load', function() {
     var response = JSON.parse(xhr.responseText);
     var matches = []; // update with appropriate json object
@@ -30,8 +38,9 @@ function sendRequest() {
   })
 }
 
-// function updateDOM(matches) {
-//   matches.forEach(function(match){
-//     document.querySelector()
-//   })
-// }
+function updateDOM(inputSaved) {
+  var mockMatch = ['beauty','beautiful','beautify','beautification','beauties'];  // remove
+  mockMatch.forEach(function(match, i){
+    suggestionElements[i].textContent = inputSaved + ' ' + match;
+  })
+}
