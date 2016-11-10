cbe0599aee43acc8722aa121499f8c984f1cbdfc
msachi <marchee88@gmail.com>
2016-11-10 13:12:33 +0000

    Add suggestion list, start working on sendRequest function

--- public/app.js
+++ public/app.js
@@ -1,8 +1,37 @@
 var inputField = document.querySelector(".input_field");
 
-inputField.onkeyup = sendRequest;
+// var suggestionElements = document.querySelectorAll(".suggestion_element");
+// for (var i = 0; i < suggestionElements.length; i++) {
+//   var suggestion = suggestion[i];
+//   button.addEventListener("click", function() {
+//     handleInput(this.id);
+//   });
+// };
+
+
+inputField.onkeyup = function() {
+  sendRequest();
+  showSuggestionList();
+}
+
+function showSuggestionList() {
+  document.querySelector(".suggestion_container").classList.remove("closed");
+}
 
 function sendRequest() {
-  var input = inputField.value;
-  console.log(input);
+  var input = inputField.value.split(' ');
+  var inputLast = input[input.length-1];
+  var url = '/' + inputLast;
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', function() {
+    var response = JSON.parse(xhr.responseText);
+    var matches = []; // update with appropriate json object
+    updateDOM(matches);
+  })
 }
+
+// function updateDOM(matches) {
+//   matches.forEach(function(match){
+//     document.querySelector()
+//   })
+// }
