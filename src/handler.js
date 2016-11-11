7bc4321c2e440172d4c3a05f7cc6f3849b417a22
marisid <sid_marina@hotmail.com>
2016-11-11 15:36:51 +0000

    add test to backend

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,11 @@
 var wordSearch = require('./wordSearch.js');
 // var dictionary = require('./readDictionary.js');
 
-var handler = function(request,response){
+var handler = function(request,response){ 
   var prefix = (request.url).split('/')[1];
   var matches = JSON.stringify(wordSearch(prefix));
   response.writeHead(200, { "content-type": "application/json"} );
   response.end(matches);
 };
+
 module.exports = handler;
