8bcd0e131ae3438184882817f7630f1ac82e1917
msachi <marchee88@gmail.com>
2016-11-11 18:36:46 +0000

    source index.html, css and app.js files by server

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,55 @@
 var wordSearch = require('./wordSearch.js');
+var fs = require('fs');
+// var StringDecoder = require('string_decoder').StringDecoder;
 // var dictionary = require('./readDictionary.js');
 
 var handler = function(request,response){
-  var prefix = (request.url).split('/')[1];
-  var matches = JSON.stringify(wordSearch(prefix));
-  response.writeHead(200, { "content-type": "application/json"} );
-  response.end(matches);
+  console.log(request.url);
+  if (request.url == '/' || request.url == '/index.html') {
+
+    fs.readFile(__dirname + '/../public/index.html','utf-8', function(error, file) {
+      console.log(file);
+    if (error) {
+      console.log (error);
+      return;
+    }
+    // var decoder = new StringDecoder('utf8');
+    response.writeHead(200, { "content-type": "text/html"} );
+    response.end(file);
+  });
+
+} else if (request.url == '/styles.css') {
+
+  fs.readFile(__dirname + '/../public/styles.css', function(error, file) {
+  if (error) {
+    console.log (error);
+    return;
+  }
+  response.writeHead(200, { "content-type": "text/css"} );
+  response.end(file);
+});
+
+} else if (request.url == '/app.js') {
+
+  fs.readFile(__dirname + '/../public/app.js', function(error, file) {
+  if (error) {
+    console.log (error);
+    return;
+  }
+  response.writeHead(200, { "content-type": "text/javascript"} );
+  response.end(file);
+});
+} else {
+  response.writeHead(404, { "content-type": "text/html" });
+  response.write('<h1>404 Page Requested Cannot be Found</h1>');
+  response.end();
+}
+
+// var prefix = (request.url).split('/')[1];
+// var matches = JSON.stringify(wordSearch(prefix));
+// response.writeHead(200, { "content-type": "application/json"} );
+// response.end(matches);
+
+
 };
 module.exports = handler;
