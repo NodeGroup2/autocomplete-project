83ba65a17c8af9782681cb99a1fa1fdcd99c296c
marisid <sid_marina@hotmail.com>
2016-11-13 22:43:41 +0000

    Change port variable to process.env.PORT for heroku deployment

--- src/server.js
+++ src/server.js
@@ -3,10 +3,11 @@ var handler = require('./handler.js');
 var dictionaryFile = require('./readDictionary.js');
 
 var server = http.createServer(handler);
+var port = process.env.PORT || 3000;
 
 function startServer() {
   dictionaryFile.readDictionary(null,null, function() {
-  server.listen(3000, function(){
+  server.listen(port, function(){
     console.log("Dictionary loaded, server listening to port 3000, ready to accept requests");
   });
 })
